6352af8a8f8fdc885779c393a8a9448162cfedfe
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2018-05-31 16:53:26 +0200

    init redux book app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: 'Javascript: the Good Parts' },
+		{ title: 'The Hitchcock\'s guide to the galaxy' },
+		{ title: 'Tri Solaris' },
+		{ title: 'The Dark Forrest' },
+	]
+}
